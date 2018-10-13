dce1e1c4e9ab004ffde0a99f10527955f7ca2d55
Jakub <kuba.lanecki@gmail.com>
2018-10-13 16:04:05 +0200

    Separated doc2vec model training and inference.

--- doc2vec/doc2vec.py
+++ doc2vec/doc2vec.py
@@ -21,66 +21,61 @@ class TaggedDocumentIterator:
         self.size = len(documents)
 
     def __iter__(self):
-        for  doc, label in zip(self._documents, self._labels):
+        for doc, label in zip(self._documents, self._labels):
             yield TaggedDocument(words=doc, tags=[label])
 
 
 class Doc2VecWrapper:
     """A class that allows to obtain vector representations for given documents."""
 
-    @staticmethod
-    def _get_representations(doc_iterator, vector_length):
-        """Trains doc2vec model on given data and returns vector representations.
+    def train(self, prep_dataset, settings):
+        """Trains doc2vec model on given data.
 
         Args:
-            doc_iterator (TaggedDocumentIterator): yields TaggedDocument instances for training.
-            vector_length (int): length of returned vector representations.
-        Returns:
-            representations (pandas.Series): NumPy arrays of vector representations for input documents.
+            prep_dataset (pandas.Series(list(str))): documents after preprocessing. They must already be tokenized.
+            settings (dict): experiment description.
 
         """
+        # Preliminaries.
+        vector_length = settings['vector_length']        
+        labels = list(prep_dataset.index)
+        doc_iterator = TaggedDocumentIterator(prep_dataset, labels)
+ 
         # Define models.
         cores = mp.cpu_count()
-        dbow_model = Doc2Vec(dm=0, vector_size=vector_length, negative=5, hs=0, min_count=2, sample=0, 
+        self._dbow_model = Doc2Vec(dm=0, vector_size=vector_length, negative=5, hs=0, min_count=2, sample=0, 
             epochs=20, workers=cores)
-        dm_model = Doc2Vec(dm=1, vector_size=vector_length, window=10, negative=5, hs=0, min_count=2, 
+        self._dm_model = Doc2Vec(dm=1, vector_size=vector_length, window=10, negative=5, hs=0, min_count=2, 
             sample=0, epochs=20, workers=cores, alpha=0.05)
     
         # Build the vocabulary.
-        dbow_model.build_vocab(doc_iterator)
-        dm_model.build_vocab(doc_iterator)
+        self._dbow_model.build_vocab(doc_iterator)
+        self._dm_model.build_vocab(doc_iterator)
 
         # Train models.
-        dbow_model.train(doc_iterator, total_examples=dbow_model.corpus_count, epochs=20)
-        dm_model.train(doc_iterator, total_examples=dm_model.corpus_count, epochs=20)
-        
-        # Get concatenated vector representations.
-        representations = []
-        for i in range(doc_iterator.size): 
-            dbow_vector = dbow_model.docvecs[i]
-            dm_vector = dm_model.docvecs[i]
-            representations.append(np.concatenate((dbow_vector, dm_vector)))
+        self._dbow_model.train(doc_iterator, total_examples=self._dbow_model.corpus_count, epochs=20)
+        self._dm_model.train(doc_iterator, total_examples=self._dm_model.corpus_count, epochs=20)
 
-        return pd.Series(representations)
+        # Discard unnecessary parameters.
+        self._dbow_model.delete_temporary_training_data(keep_doctags_vectors=False)
+        self._dm_model.delete_temporary_training_data(keep_doctags_vectors=False)
 
-    @classmethod
-    def doc2vec_features(cls, prep_dataset, settings):
+    def doc2vec_features(self, prep_dataset):
         """Returns doc2vec vector representations for given documents.
  
         Args:
             prep_dataset (pandas.Series(list(str))): documents after preprocessing. They must already be tokenized.
-            settings (dict): experiment description.
         Returns:
             representations (pandas.Series): NumPy arrays of vector representations for input documents.
 
         """
-        # Some preliminaries.
-        vector_length = settings['vector_length']        
-        labels = list(prep_dataset.index)
-        doc_iterator = TaggedDocumentIterator(prep_dataset, labels)
-        
-        # Get vector representations.
-        representations = cls._get_representations(doc_iterator, vector_length)
+        # Get vector representations from both models.
+        # BTW inference parameters (alpha, number of epochs and so on) can be changed here.
+        dbow_rep = prep_dataset.apply(self._dbow_model.infer_vector)
+        dm_rep = prep_dataset.apply(self._dm_model.infer_vector)
+    
+        # Concatenate obtained representations.
+        representations = dbow_rep.combine(dm_rep, lambda r1, r2: np.concatenate((r1, r2)))
 
         return representations
 
